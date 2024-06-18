ec8b6591362496fdb23e6d51fbfeca9b380d6308
Tushar <tushar@fishbrain.com>
2024-06-18 10:46:53 +0200

    Add field to Msg Data only if sent from bonito

--- fcm_firbase_admin.go
+++ fcm_firbase_admin.go
@@ -15,74 +15,61 @@ func (this *FcmMsg) makeMulticastMessageData() (*map[string]string, bool) {
 	}
 
 	data, ok := this.Data.(map[string]interface{})
+	dataMap := make(map[string]string)
 	if !ok {
 		return nil, false
 	}
 
-	var (
-		title                     string
-		body                      string
-		itemType                  string
-		itemID                    string
-		deepLink                  string
-		imageURL                  string
-		sound                     string
-		actorNickname             string
-		badgeCount                int
-		actionsSerialized         string
-		trackingPayloadSerialized string
-	)
-
 	titleField, ok := data["title"]
 	if ok {
-		title = titleField.(string)
+		dataMap["title"] = titleField.(string)
 	}
 
 	bodyField, ok := data["body"]
 	if ok {
-		body = bodyField.(string)
+		dataMap["body"] = bodyField.(string)
 	}
 
 	itemTypeField, ok := data["item_type"]
 	if ok {
-		itemType = itemTypeField.(string)
+		dataMap["item_type"] = itemTypeField.(string)
 	}
 
 	itemIdField, ok := data["item_id"]
 	if ok {
-		itemID = itemIdField.(string)
+		dataMap["item_id"] = itemIdField.(string)
 	}
 
 	deepLinkField, ok := data["deeplink"]
 	if ok {
-		deepLink = deepLinkField.(string)
+		dataMap["deeplink"] = deepLinkField.(string)
 	}
 
 	imageUrlField, ok := data["image_url"]
 	if ok {
-		imageURL = imageUrlField.(string)
+		dataMap["image_url"] = imageUrlField.(string)
 	}
 
 	soundField, ok := data["sound"]
 	if ok {
-		sound = soundField.(string)
+		dataMap["sound"] = soundField.(string)
 	}
 
 	actorNicknameField, ok := data["actor_nickname"]
 	if ok {
-		actorNickname = actorNicknameField.(string)
+		dataMap["actor_nickname"] = actorNicknameField.(string)
 	}
 
 	badgeCountField, ok := data["badge_count"]
 	if ok {
-		badgeCount = int(badgeCountField.(float64))
+		dataMap["badge_count"] = strconv.Itoa(int(badgeCountField.(float64)))
 	}
 
 	actionsField, ok := data["actions"]
 	if ok {
 		bytes, err := json.Marshal(actionsField)
 		if err == nil {
-			actionsSerialized = string(bytes)
+			dataMap["actions"] = string(bytes)
 		}
 	}
 
@@ -90,22 +77,9 @@ func (this *FcmMsg) makeMulticastMessageData() (*map[string]string, bool) {
 	if ok {
 		bytes, err := json.Marshal(trackingPayloadField)
 		if err == nil {
-			trackingPayloadSerialized = string(bytes)
+			dataMap["tracking_payload"] = string(bytes)
 		}
 	}
-
-	dataMap := make(map[string]string)
-	dataMap["title"] = title
-	dataMap["body"] = body
-	dataMap["item_type"] = itemType
-	dataMap["item_id"] = itemID
-	dataMap["deeplink"] = deepLink
-	dataMap["image_url"] = imageURL
-	dataMap["sound"] = sound
-	dataMap["actor_nickname"] = actorNickname
-	dataMap["badge_count"] = strconv.Itoa(badgeCount)
-	dataMap["actions"] = actionsSerialized
-	dataMap["tracking_payload"] = trackingPayloadSerialized
 
 	return &dataMap, true
 }
